75cc6900889f239de3ec86fd63367373606d8112
Sapna Maurya <sapnassm62062@gmail.com>
2026-08-01 15:35:46 +0530

    debug mongo uri

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
     try {
 
+        console.log("MONGO URI:", process.env.MONGO_URI);
+
         await mongoose.connect(process.env.MONGO_URI);
 
         console.log("MongoDB Connected Successfully ✅");
